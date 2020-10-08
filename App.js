39ff06d54240f87a2d62cf4751bbfa652d10cdd3
Kevin Chacon <kevinchacong95@gmail.com>
2020-10-08 00:42:41 -0300

    Module app done

--- App.js
+++ App.js
@@ -8,5 +8,43 @@ function saveTask (e) {
         title,
         description
     }
+    if(localStorage.getItem('tasks') === null){
+        let tasks = []
+        tasks.push(task)
+        localStorage.setItem('tasks',JSON.stringify(tasks))
+
+    } else {
+        let tasks = JSON.parse(localStorage.getItem('tasks'))
+        tasks.push(task)
+        localStorage.setItem('tasks',JSON.stringify(tasks))
+    }
+    getTask()
+    document.getElementById('formTask').reset()
     e.preventDefault()
+}
+
+function getTask() {
+    let tasks = JSON.parse(localStorage.getItem('tasks'))
+    let tasksView = document.getElementById('tasks')
+
+    tasksView.innerHTML = ''
+    for (let i = 0 ;i<tasks.length;i++){
+        tasksView.innerHTML += `<div class="card mb-4">
+            <div class="card-body">
+                <p>${tasks[i].title} - ${tasks[i].description}</p>
+                <a class="btn btn-danger" onclick="deleteTask('${tasks[i].title}')">Delete</a>
+            </div>
+        </div>`
+    }
+}
+
+function deleteTask(title){
+    let tasks = JSON.parse(localStorage.getItem('tasks'))
+
+    for(let i = 0; i<tasks.length ;i++){
+        if(tasks[i].title == title)
+            tasks.splice(i,1)
+    }
+    localStorage.setItem('tasks',JSON.stringify(tasks))
+    getTask()
 }
